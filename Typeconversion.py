d917f6f7a12afafa066f851b95fdeaaf32aaa8bc
Diamond Kumar Shah <daymandkumarsah@gmail.com>
2025-09-21 13:11:15 +0530

    Comment out code in Typeconversion.py and add input.py
    Commented out previously active code in Typeconversion.py for type conversion examples. Added input.py with various examples demonstrating user input handling and type conversion in Python.

--- Typeconversion.py
+++ Typeconversion.py
@@ -6,13 +6,13 @@
 # print(sum)
 # print(type(sum)) #veriable a is autometicaly converted to float
 # print(int(b))
-a="8"
+# a="8"
 # c=int(a) #veriable a is converted to int
 # b=5
 # sum=int(a)+b #veriable a is converted to int 
 # print(sum)
 # print(type(c))
 # print(type(a))
-d=float(a) #veriable a is converted to float
-print(d)
-print(type(d))
+# d=float(a) #veriable a is converted to float
+# print(d)
+# print(type(d))
---
+++ input.py
@@ -0,0 +1,25 @@
+#input in python
+# a=7
+# b=9
+# sum=a+b
+# print("sum of both number =",sum)
+
+
+# a=input("Enter your first Number :")
+# b=input("Enter your Second Number :")
+# sum=a+b
+# print("sum of both number =",sum)
+
+# a=int(input("Enter your first Number :"))
+# b=int(input("Enter your Second Number :"))
+# sum=a+b
+# print("sum of both number =",sum)
+
+# a=input("Enter your Name :")
+# b=input("Enter your Age :")
+# print("My name is",a,"and my age is",b)
+
+a=input("Enter your first Number : ")
+b=input("Enter your Second Number : ")
+sum=int(a)+int(b)
+print("sum of both number=",sum)
