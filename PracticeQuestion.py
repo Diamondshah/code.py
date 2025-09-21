f83a25c669813f357c850363d1a2902755ede253
Diamond Kumar Shah <daymandkumarsah@gmail.com>
2025-09-21 13:34:03 +0530

    Add program to calculate area of a square
    Commented out the previous sum calculation code and added a new program that takes the side of a square as input and prints its area.

--- PracticeQuestion.py
+++ PracticeQuestion.py
@@ -1,6 +1,13 @@
 #Q1.... write a program to input 2 numbers and print their sum
 
-a=int(input("Enter karo pahla Number : "))
-b=int(input("Enter karo dusra Number : "))
-sum=a+b
-print("sum of both number =",sum)
+# a=int(input("Enter karo pahla Number : "))
+# b=int(input("Enter karo dusra Number : "))
+# sum=a+b
+# print("sum of both number =",sum)
+
+#Q2...WAP to input side of a square and print area of squear
+
+side=float(input("Enter Side of a Square(cm) :"))
+# area=side*side
+area=side**2
+print("Area of your Square :",area)
