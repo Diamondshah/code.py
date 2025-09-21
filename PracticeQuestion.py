646138ec3c4411120d85a45c2dbea4ba33caafb6
Diamond Kumar Shah <daymandkumarsah@gmail.com>
2025-09-21 13:24:23 +0530

    Add program to sum two input numbers
    Introduced PracticeQuestion.py with a script that prompts the user for two numbers and prints their sum.

---
+++ PracticeQuestion.py
@@ -0,0 +1,6 @@
+#Q1.... write a program to input 2 numbers and print their sum
+
+a=int(input("Enter karo pahla Number : "))
+b=int(input("Enter karo dusra Number : "))
+sum=a+b
+print("sum of both number =",sum)
