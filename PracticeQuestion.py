7c478a0e440bdf29eb6cd823830246fd536e8098
Diamond Kumar Shah <daymandkumarsah@gmail.com>
2025-09-21 13:55:28 +0530

    Add solution for comparing two numbers
    Commented out previous solutions for square area and average calculation. Added code to input two numbers and print True if the first is less than or equal to the second, otherwise False.

--- PracticeQuestion.py
+++ PracticeQuestion.py
@@ -7,7 +7,21 @@
 
 #Q2...WAP to input side of a square and print area of squear
 
-side=float(input("Enter Side of a Square(cm) :"))
-# area=side*side
-area=side**2
-print("Area of your Square :",area)
+# side=float(input("Enter Side of a Square(cm) :"))
+# # area=side*side
+# area=side**2
+# print("Area of your Square :",area)
+
+#Q3... WAP to input 2 Floating Number and print their average
+
+# a=float(input("Enter First Floating Number :"))
+# b=float(input("Enter Second Floating Number :"))
+# sum=a+b
+# average=sum/2
+# print("Average of those Number : ",average)
+
+"""Q4... WAP to input 2 Number and print ture if a<=b efse false"""
+
+a=int(input("Enter your first Num a : "))
+b=int(input("Enter your secand Num b :"))
+print(a<=b)
