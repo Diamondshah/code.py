5a5f489baa30ae9faa8d439a13c495fe81b4a25a
Diamond Kumar Shah <daymandkumarsah@gmail.com>
2025-10-07 19:08:25 +0530

    Add nested if-else example for eligibility checks
    Commented out the grading system code and added a new example demonstrating nested if-else statements to check eligibility for voting, driving, and marriage based on age input.

--- Lecture 2/elif.py
+++ Lecture 2/elif.py
@@ -11,14 +11,30 @@
 #     print("b is Greater than a")
 
 #Q..... Grading System
-marks=int(input("Enter your Marks :  "))
-if(marks>=90 and marks<=100):
-    print("Your Grade is A+")
-elif(marks>=80 and marks<90):
-    print("Your Grade is A")
-elif(marks>=70 and marks<80):
-    print("Your Grade is B+")
-elif(marks>=60 and marks<70):
-    print("Your Grade is B")
+# marks=int(input("Enter your Marks :  "))
+# if(marks>=90 and marks<=100):
+#     print("Your Grade is A+")
+# elif(marks>=80 and marks<90):
+#     print("Your Grade is A")
+# elif(marks>=70 and marks<80):
+#     print("Your Grade is B+")
+# elif(marks>=60 and marks<70):
+#     print("Your Grade is B")
+# else:
+#      print("You are Fail , Try Again")   
+
+
+#Nested if else
+age=int(input("Enter your Age :"))
+if age>=18:
+    print("You are Eligible to Voting")
+    if age>=21:
+        print("You are Eligible for Driving")
+        if age>=24:
+            print("You are Eligible for Marriage")
+        else:
+            print("You are not Eligible for Marriage")
+    else:
+        print("You are not Eligible for Driving")
 else:
-        print("You are Fail , Try Again")   
+    print("You are not Eligible to Voting")
