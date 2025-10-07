5a9b3caa883d00ecdf1b0c200dc6f8a46ba01130
Diamond Kumar Shah <daymandkumarsah@gmail.com>
2025-10-07 11:05:58 +0530

    Add string indexing and slicing examples
    Expanded String.py with examples and explanations of string indexing, negative indexing, and various slicing techniques. Added tempCodeRunnerFile.py to demonstrate that string item assignment is not supported.

--- Lecture 2/String.py
+++ Lecture 2/String.py
@@ -59,3 +59,45 @@
 # len() function is used to find the length of a string
 # a="Hello World"
 # print(len(a))
+
+#indexing in String
+# Indexing means accessing individual characters of a string
+# Indexing starts from 0
+# a="Diamond"
+# print(a[0])
+# print(a[1])
+# print(a[2])
+# bro=a[5]
+# print(bro)
+# a[8]='P'   not supported
+
+#Negative Indexing
+# Negative indexing means accessing individual characters of a string from the end
+# a="Diamond"
+# print(a[-1])
+# print(a[-2])
+# print(a[-3])
+# bro=a[-4]
+# print(bro)
+# # a[-5]='P'   #not supported
+
+#Accessing a range of characters in a string
+a="Diamond"
+print(a[0:3])  #0 to 2
+print(a[1:5])  #1 to 4
+print(a[2:])   #2 to end
+print(a[:4])   #0 to 3
+print(a[:])    #0 to end
+print(a[1:6:2]) #1 to 5 with step 2
+print(a[::2])   #0 to end with step 2
+print(a[::-1])  #reverse the string
+print(a[-1:-6:-1]) #reverse the string from index -1 to -5
+print(a[-3:-8:-1]) #reverse the string from index -3 to
+print(a[-5:-1])   #from index -5 to -2
+print(a[-6:])    #from index -6 to end
+print(a[:-2])    #from index 0 to -3
+print(a[-6:-1])  #from index -6 to -2
+print(a[-7:-1])  #from index -7 to -2
+print(a[-7:5])   #from index -7 to 4
+print(a[-7:6])   #from index -7 to 5
+
