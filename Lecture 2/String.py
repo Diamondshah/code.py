bf02cb4995845b6002166a83bff2a0cbe9449b9e
Diamond Kumar Shah <daymandkumarsah@gmail.com>
2025-10-07 10:25:31 +0530

    Expand string handling examples and explanations
    Added detailed comments and examples on string creation, escape sequences, and concatenation in Python. Enhanced the script to demonstrate different ways to define strings, use escape characters, and concatenate strings for educational clarity.

--- Lecture 2/String.py
+++ Lecture 2/String.py
@@ -1,14 +1,49 @@
 #String..........................................
+# String is a sequence of characters
+# How to Write a String in Python
+# There are three ways to write a string in Python:
+# a="Hello"
+# b='Hello'
+# c='''Hello'''
+# d="""Hello"""
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
 
+#Escape Sequence Characters
+# \n=New Line
+# \t=Tab
+# \\=Backslash
+# \'=Single Quote
+# \"=Double Quote
+# Examples of Escape Sequence Characters
+
+
+# a="Hello \nWorld"
+# b="Hello \tWorld"
+# c="Hello \\ World"
+# d='Hello \'World\''
+# e="Hello \"World\""
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+
+#String Concatenation
 a="Hello"
-b='Hello'
-c='''Hello'''
-d="""Hello"""
-print(a)
-print(b)
+b="World"
+c=a+" "+b
 print(c)
-print(d)
-print(type(a))
-print(type(b))
 print(type(c))
-print(type(d))
+print(a+b)
