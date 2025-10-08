7214c9c0397b8fdef52642bfbf88efe7f8b29037
Diamond Kumar Shah <daymandkumarsah@gmail.com>
2025-10-08 20:51:33 +0530

    Add introductory notes on Python tuples
    Created Tuple.py with comments explaining the properties, usage, and characteristics of tuples in Python.

---
+++ List & Tuple/Tuple.py
@@ -0,0 +1,17 @@
+# Tuple .........()....
+
+# A tuple is a collection which is ordered and unchangeable.
+#  In Python tuples are written with round brackets.
+# Tuples are used to store multiple items in a single variable.
+# Tuple allow duplicate values.
+#  Tuple are faster than list.
+#  Tuple can store elements of different types (integer,float,string,etc.)
+#  Tuple can be nested.
+#  Tuple can be converted to list and vice versa.
+#  Tuple are immutable.
+#  Tuple are hashable.
+#  Tuple are iterable.
+#  Tuple are used to store data that should not be changed.
+#
+
+
