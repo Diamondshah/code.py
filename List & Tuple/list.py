0be1101b13115192c0073c741b477a5b92e7b59c
Diamond Kumar Shah <daymandkumarsah@gmail.com>
2025-10-08 20:08:18 +0530

    Add examples demonstrating Python list operations
    Introduces a new script with examples of list creation, indexing, slicing, and common list methods such as append, sort, and reverse. The code includes comments explaining list features and demonstrates usage with sample data.

---
+++ List & Tuple/list.py
@@ -0,0 +1,42 @@
+# LIST......[]...........
+# A Buil in data type that stores set of value 
+# It Can Store Elements of Different types[integer,flat,string,etc.]
+
+# my_list=[35,54,55.2,454,148]
+# print(my_list[4])
+# print(my_list[3])
+# print(my_list[1])
+# print(type(my_list[2]))
+# print(type(my_list))
+
+# all=["hello",'H',35,1245.00,"Kese ho "]
+# print(all[1])
+# print(all[3])
+# print(all[0])
+# print(all[4])
+
+# print(type(all[1]))
+# print(type(all[3]))
+# print(type(all[0]))
+# print(type(all[4]))
+# all[1]="Diamond"
+# print(all)
+
+
+#list Sliding..........
+
+# print(all[1:4])
+# print(all[-3:-1])
+
+# List of Method................
+
+nunu=[49,65,21,85,64,15,24,33,54,25]
+# print(nunu)
+# print(nunu[4:8])
+# nunu.append(99)
+# print(nunu)
+# print(nunu.sort())
+# nunu.sort()
+# nunu.sort(reverse=True)
+nunu.reverse()
+print(nunu)
