bb002ec4a9123cf4d728759b7424e393af1a0a59
Diamond Kumar Shah <daymandkumarsah@gmail.com>
2025-10-10 18:33:31 +0530

    Add example of nested dictionary access
    Commented out the previous dictionary examples and added a new example demonstrating how to access values in a nested dictionary. The code now prints the value of 'math' from the 'sub' dictionary inside 'Result'.

--- Dictionary &  Set/Dictionary.py
+++ Dictionary &  Set/Dictionary.py
@@ -4,20 +4,36 @@
 #  and they have keys and values.
 #Creating a Dictionary
 #Dictionary={key:Value,Kay:Value,Key:Value,Key:Value,Key:Value}
-no_frnd={}
-frnd={
-    "info":["name","age"],
-    "type":("str","inteager"),
-    "Prince":21.5,
-    "avisash":19,
-    "Diamond":20,
-    "Rohit":19,
-    "Nitesh":20
-}
-print(type(no_frnd))
+# no_frnd={}
+# frnd={
+#     "info":["name","age"],
+#     "type":("str","inteager"),
+#     "Prince":21.5,
+#     "avisash":19,
+#     "Diamond":20,
+#     "Rohit":19,
+#     "Nitesh":20
+# }
+# print(type(no_frnd))
 # frnd["yaksh"]=22
 # frnd["Rohit"]=20
 # print(frnd)
 # print(frnd["Diamond"])
 # print(frnd["Rohit"])
 # print(frnd["Nitesh"])
+
+
+#Nested Dictionary...........
+Result={
+    "name":"Prakash",
+    "sub":{
+        "phy":48,
+        "math":98,
+        "Hindi":68,
+        "English":78,
+    }
+}
+# print(Result)
+# print(Result[sub["math"]])    # ✖ method
+
+print(Result["sub"]["math"])
