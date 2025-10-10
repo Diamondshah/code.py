26bc0f568fea3aed94617cd945820e9baed52423
Diamond Kumar Shah <daymandkumarsah@gmail.com>
2025-10-10 16:05:33 +0530

    Add example of Python dictionary usage
    Introduces a sample Python script demonstrating dictionary creation and basic usage. The script defines a dictionary of friends and attempts to print an element, though the usage of frnd(2) is incorrect and may raise an error.

---
+++ Dictionary &  Set/Dictionary.py
@@ -0,0 +1,16 @@
+#Dictionary......in Python 
+#A dictionary is a collection which is unordered, 
+# changeable and indexed. In Python dictionaries are written with curly brackets,
+#  and they have keys and values.
+#Creating a Dictionary
+#Dictionary={key:Value,Kay:Value,Key:Value,Key:Value,Key:Value}
+
+frnd={
+    "Prince":21,
+    "avisash":19,
+    "Diamond":20,
+    "Rohit":19,
+    "Nitesh":20
+}
+print(frnd(2))
+# print(frnd)
