1d9407b388c93b3ccb5490b21179f5ed56d022f9
Diamond Kumar Shah <daymandkumarsah@gmail.com>
2025-10-11 17:37:52 +0530

    Add loops example and comment out set question code
    Added a new file Loop/loops.py demonstrating while loops in Python with examples. Commented out the code for the set question in PracticeQuestion.py, leaving only the question and commented code.

---
+++ Loop/loops.py
@@ -0,0 +1,19 @@
+#loops.........
+#loop is used to repeat a block of code multiple
+#  times until a certain condition is met.
+# loops are use to repeart instructions.
+# Types of loops in python 
+# 1...while loop      2....For loop
+
+# while loop.......(jab-tak)
+# num=15
+# while 0<=num:
+#     print(num,"ruko jara sabar karo .")
+#     num=num-1
+
+# print 1 to 9 number 
+num=1
+while num<=9:
+    print(num)
+    num=num+1
+
