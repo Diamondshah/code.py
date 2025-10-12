e1c22b803961a101bdf86189e4fecab232ab24a2
Diamond Kumar Shah <daymandkumarsah@gmail.com>
2025-10-12 12:59:52 +0530

    Add search for number X in tuple using loop
    Replaces the previous list printing code with a loop that searches for a user-provided number X in a tuple and prints the index if found.

--- Loop/loops.py
+++ Loop/loops.py
@@ -38,14 +38,22 @@
 #Q....Print the elements of the following list using a loop
 
 
-list=[1,4,9,16,25,36,49,64,81,100]
-n=0
-while n<=len(list):
-    print(list[n])
-    n=n+1
+# list=[1,4,9,16,25,36,49,64,81,100]
+# n=0
+# while n<=len(list):
+#     print(list[n])
+#     n=n+1
 
 # num=1
 # list={}
 # while num<=10:
 #     list{print(num**2)}
 #     num=num+1
+# Search for a number X in this tuple using loop:
+list=(1,4,9,16,25,36,49,64,81,100)
+x=int(input("Enter the value of X "))
+n=0
+while n < len(list) :
+    if(x==list[n]):
+        print(n,"found")
+    n=n+1
