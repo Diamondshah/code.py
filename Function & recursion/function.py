19e14726e3c80400ecc01b2a148fa81cfb6111b4
Diamond Kumar Shah <daymandkumarsah@gmail.com>
2025-10-15 08:04:42 +0530

    Add function to print list elements in a single line
    Introduced the print_sline function to print all elements of a list in a single line, and added example usage with m_frnd. Also added tempCodeRunnerFile.py for testing print_sline with frnd.

--- Function & recursion/function.py
+++ Function & recursion/function.py
@@ -6,10 +6,64 @@
 func_name(arg1,arg2...)#function call
 """
 # Q create a function of add 2 number 
-def cal_sum(a,b):
-    sum=a+b
-    print(sum)
-    return sum
+#Function definition
+# def cal_sum(a,b):#a,b are parameters
+#     sum=a+b
+#     print(sum)
+#     return sum
 
-cal_sum(4,5)
-cal_sum(11,85)
+# cal_sum(4,5)#4,5 are arguments.
+# cal_sum(11,85)#11,85 are arguments.
+
+# Q... its a function print hello
+# def phlo():
+#     print("hello")
+
+# hlo=phlo()
+# print(hlo)
+# phlo()
+# phlo()
+
+#Q...Average of 3 Number...
+# def avg(a,b,c):
+#     sum=a+b+c
+#     average=sum/3
+#     print("average of a,b and c :",average)
+#     return average
+
+# a=
+# b=int(input("Enter your b number"))
+# c=int(input("Enter your c number"))
+# average=avg(a,b,c)
+# print(average)
+
+#type of function in [python]
+#1..built in function :- len(),range(),type(),print()...etc
+#2...user defined function :- avg(),sum(),...jo user banata hai ..
+# print("Praksh" , end=" & ")
+# print("kumar")
+#Q... Create a funcvtion to print the length of list 
+
+# frnd=["kaluaa","prince","Rohit","nitesh","akash","Niraj","avinash","Priyanshu","krishna","sunny"]
+# m_frnd=["RAjveer","Mandeep","priya","falak","dilkash","ishika","mamta","puja","Gunvir","sanya","bhumika"]
+# def print_len(list):
+#     print(len(list))
+
+# print_len(frnd)
+# print_len(m_frnd)
+
+#Q....WAF to print the element of a list in a single line.(list os the parameter)
+frnd=["kaluaa","prince","Rohit","nitesh","akash","Niraj","avinash","Priyanshu","krishna","sunny"]
+m_frnd=["RAjveer","Mandeep","priya","falak","dilkash","ishika","mamta","puja","Gunvir","sanya","bhumika"]
+
+def print_sline(list):
+    num=len(list)
+    n=0
+    for n in num:
+#     while n<num:
+        print(list[n],end=",")
+        n=n+1
+
+# print_sline(frnd)
+print_sline(m_frnd)
+
