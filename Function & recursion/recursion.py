792e83612adce2578207bcf054475908d71772f3
Diamond Kumar Shah <daymandkumarsah@gmail.com>
2025-10-17 07:32:06 +0530

    Add recursive function to print all elements in a list
    Commented out the previous sum calculation function and added a new recursive function, print_list, which prints each element of a list using recursion. Demonstrated usage with a sample list of names.

--- Function & recursion/recursion.py
+++ Function & recursion/recursion.py
@@ -12,10 +12,20 @@
 # print(fact(n))
 
 #Q...Write a recursive function to calculate the sum of first n natural numbers.
-def calc_sum(n):
-    if(n==0):
-        return 0
-    return calc_sum(n-1)+n
+# def calc_sum(n):
+#     if(n==0):
+#         return 0
+#     return calc_sum(n-1)+n
 
-sum=calc_sum(10)
-print(sum)
+# sum=calc_sum(10)
+# print(sum)
+
+#Q....Write a recursive function to print all element in a list.(hint:use list & index as parameters.)
+def print_list(list,idx=0 ):
+    if(idx==len(list)):
+        return
+    print(list[idx])
+    print_list(list,idx+1)
+
+frnd=["kaluaa","prince","Rohit","nitesh","akash","Niraj","avinash","Priyanshu","krishna","sunny"]
+print_list(frnd,)
