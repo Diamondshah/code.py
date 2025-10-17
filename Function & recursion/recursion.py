9d731cb6358a155deb5d606103b77d110a826c40
Diamond Kumar Shah <daymandkumarsah@gmail.com>
2025-10-17 07:14:49 +0530

    Add recursion example for factorial calculation
    Added commented code demonstrating how to calculate factorial using recursion in Python. Includes explanatory comments and sample input/output code.

---
+++ Function & recursion/recursion.py
@@ -0,0 +1,13 @@
+#......Recursion.....
+#When a function calls itself repeatedly
+
+
+#Q... fectorial calculate...   #...❌ aabhi samajhna hai
+# def fact(n):
+#     if (n==1 or n==0):
+#         return 1
+#     return fact(n-1)*n              
+
+# n=int(input("Enter the Value of n:"))
+# print(fact(n))
+
