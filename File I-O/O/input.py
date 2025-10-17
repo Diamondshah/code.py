48cf26591ac30350e96b43972d5d84c31e824763
Diamond Kumar Shah <daymandkumarsah@gmail.com>
2025-10-17 07:38:23 +0530

    Add introductory comments on file I/O in Python
    Created input.py with comments explaining file I/O operations in Python and the types of files (text and binary) that can be handled.

---
+++ File I-O/O/input.py
@@ -0,0 +1,5 @@
+#File I/O in Python...
+#python can be used to perform operation on a file.(read&write data)
+#Type of all files:-
+# 1. Text Files:. .txt,.doc,.log...etc.
+# 2. Binary Files:. .mp4, .mov, .png, .jpeg etc.
