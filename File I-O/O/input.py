65512807ff46db104e3c4dd0c8283983f2b388a0
Diamond Kumar Shah <daymandkumarsah@gmail.com>
2025-10-23 19:37:22 +0530

    Add file reading example and demo files
    Updated input.py to demonstrate reading from a file and printing its contents, length, and type. Added Timepass.py with simple print statements and demo.txt as a sample text file for file operations.

--- File I-O/O/input.py
+++ File I-O/O/input.py
@@ -3,3 +3,20 @@
 #Type of all files:-
 # 1. Text Files:. .txt,.doc,.log...etc.
 # 2. Binary Files:. .mp4, .mov, .png, .jpeg etc.
+
+
+#open,Read and Close a file.....
+#We have toi open a file before raeding or writing....
+#f=open("file_name","mode")
+#fiel_name>>.>>             #mode
+#sample.txt              r:read mode
+#demo.docx                w: write mode
+#data=f.read()
+#f.close(q)
+f=open("demo.txt","r")
+data=f.read()
+# print(data)
+print(len(data))
+print(type(data))
+print(data)
+f.close()
